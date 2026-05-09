e1d483dced92df129c77a224320b5d287956f5ff
Thanin-lim <145980618+Thanin-lim@users.noreply.github.com>
2026-05-09 09:13:30 +0700

    [TEST] CI&CD

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello from Flask in Docker!</h1><p>My Airflow project is working.</p>"
+    return "<h1>Hello from test in Docker!</h1><p>My Airflow project is working.</p>"
 
 if __name__ == '__main__':
-    # ต้องตั้ง host เป็น 0.0.0.0 เพื่อให้เข้าถึงจากนอก Docker ได้
     app.run(debug=True, host='0.0.0.0', port=5000)
